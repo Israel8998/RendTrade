6a4df010dc3c701e72e8fba6c3ecb0e3a73e7ad4
Israel <israel_pazmi@hotmail.com>
2022-09-06 11:11:10 -0500

    inicio

---
+++ app/src/main/java/com/rendimiento/rendtrade/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rendimiento.rendtrade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
